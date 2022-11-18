dd578da2686557791a6e57196a126059fe39163f
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-18 15:55:17 +0100

    First Commit

---
+++ src/main/java/it/prova/myebay/MyebayApplication.java
@@ -0,0 +1,13 @@
+package it.prova.myebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyebayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyebayApplication.class, args);
+	}
+
+}
